43cbd6311600179fb58af5d80b1891e9a296a3c1
naamacosta <naamacosta@gmail.com>
2025-03-17 21:44:34 -0300

    Bancario1

---
+++ APIBancario/src/test/java/com/example/APIBancario/ApiBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIBancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
